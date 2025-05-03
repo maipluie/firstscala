d600ba6d56cc22d1d714bea08a5fd3bcde1ec5fe
maipluie <takanisimura@gmail.com>
2025-05-03 18:23:59 +0900

    test

--- test/SampleClass1Test.scala
+++ test/SampleClass1Test.scala
@@ -4,6 +4,6 @@ import org.scalatest.FunSuite
 class SampleClass1Test extends FunSuite{
 	test("SampleClass1.case1"){
 	    def c = new SampleClass1()
-		assert(c.inclementTwo(3) == 5)
+		assert(c.inclementTwo(3) === 6)
 	}
 }
